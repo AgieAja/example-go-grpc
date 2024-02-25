b822badd844ae1bf620a6603d76c449269b2a25e
AgieAja <38944947+AgieAja@users.noreply.github.com>
2024-02-25 11:42:24 +0700

    [FEATURE] - add get product

--- main.go
+++ main.go
@@ -7,18 +7,40 @@ import (
 	"net"
 
 	pb "github.com/AgieAja/example-go-grpc/examplepb/proto"
+	pbProduct "github.com/AgieAja/example-go-grpc/productpb/proto"
 
 	"google.golang.org/grpc"
 )
 
 type server struct {
 	pb.UnimplementedGreetingServiceServer
+	pbProduct.UnimplementedProductServiceServer
 }
 
 func (s *server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
 	name := in.GetName()
 	message := fmt.Sprintf("Hello, %s!", name)
 	return &pb.GreetResponse{Message: message}, nil
+}
+
+func (s *server) GetProduct(ctx context.Context, in *pbProduct.Empty) (*pbProduct.GetProductsResponse, error) {
+	response := &pbProduct.GetProductsResponse{
+		Products: []*pbProduct.GetProductResponse{
+			{
+				Id:   1,
+				Name: "Product 1",
+			},
+			{
+				Id:   2,
+				Name: "Product 2",
+			},
+			{
+				Id:   3,
+				Name: "Product 3",
+			},
+		},
+	}
+	return response, nil
 }
 
 func main() {
@@ -28,6 +50,7 @@ func main() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterGreetingServiceServer(s, &server{})
+	pbProduct.RegisterProductServiceServer(s, &server{})
 	log.Println("server started")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
